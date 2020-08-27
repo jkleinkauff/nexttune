e8c73928ed8fc9e66ecb54f30c95906fb3992cf4
Jhonatas <kleinkauff@gmail.com>
2020-08-26 23:58:05 -0300

    remove some comments

--- api/main.py
+++ api/main.py
@@ -20,7 +20,6 @@ class CustomApi(Api):
 
 
 def create_app(test_config=None):
-    """Create and configure an instance of the Flask application."""
     app = Flask(__name__)
     api = CustomApi(app)
 
