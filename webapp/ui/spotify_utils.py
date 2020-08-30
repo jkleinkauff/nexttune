6c5d90e883c50f85127f8fd600b9aa49b0182a63
Jhonatas <kleinkauff@gmail.com>
2020-08-30 14:41:03 -0300

    add new apps

--- webapp/ui/spotify_utils.py
+++ webapp/ui/spotify_utils.py
@@ -25,19 +25,16 @@ def get_token(session):
     token_valid = False
     token_info = session.get("token_info", {})
 
-    # Checking if the session already has a token stored
+    # token already stored?
     if not (session.get("token_info", False)):
         token_valid = False
         return token_info, token_valid
 
-    # Checking if token has expired
-    # now = int(time.time())
+    # token expired ?
     is_token_expired = False
     is_token_expired = session.get("token_info").get("expires_at") - time.time() < 60
 
-    # Refreshing token if it has expired
     if is_token_expired:
-        # Don't reuse a SpotifyOAuth object because they store token info and you could leak user tokens if you reuse a SpotifyOAuth object
         sp_oauth = spotipy.oauth2.SpotifyOAuth(
             client_id=settings.SPOTIFY_APP_CLI_ID,
             client_secret=settings.SPOTIFY_APP_CLI_SEC,
