89e3371f3685937d98a660e9e3fe0be183238c23
Jhonatas <kleinkauff@gmail.com>
2020-08-30 14:41:03 -0300

    add new apps

---
+++ webapp/current_playing/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class CurrentPlayingConfig(AppConfig):
+    name = 'current_playing'
---
+++ webapp/top_tracks/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class TopTracksConfig(AppConfig):
+    name = 'top_tracks'
